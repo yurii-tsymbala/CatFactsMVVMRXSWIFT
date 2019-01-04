b6fd82a32801fa149f05053b90817785e6b99de4
Yurii Tsymbala <zumbala2013@gmail.com>
2019-01-04 17:50:45 +0200

    Add Pods Setup packages

---
+++ Source/Networking/DownloadService.swift
@@ -0,0 +1,32 @@
+//
+//  DownloadService.swift
+//  CatFacts
+//
+//  Created by Yurii Tsymbala on 1/4/19.
+//  Copyright © 2019 Yurii Tsymbala. All rights reserved.
+//
+
+import Foundation
+
+enum DownloadServiceError: Error {
+  case firstError
+  case secondError
+  case thirdError
+  case fourthError
+  case fifthError
+  case sixthError
+  case seventhError
+  case eighthError
+  case ninthError
+  case tenthError
+  case eleventhError
+}
+
+protocol DownloadServiceType {
+
+}
+
+class DownloadService: DownloadServiceType {
+  
+}
+
---
+++ Source/ViewModel/SignInViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SignInViewModel.swift
+//  CatFacts
+//
+//  Created by Yurii Tsymbala on 1/4/19.
+//  Copyright © 2019 Yurii Tsymbala. All rights reserved.
+//
+
+import Foundation
